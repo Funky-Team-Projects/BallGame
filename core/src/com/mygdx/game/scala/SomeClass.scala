93b21197c8f4fa7b6ac7636246c2c604186649b5
Vladislav <Streamline28@mail.ru>
2015-03-02 18:05:37 +0200

    Initial commit

---
+++ core/src/com/mygdx/game/scala/SomeClass.scala
@@ -0,0 +1,11 @@
+package com.mygdx.game.scala
+
+import com.badlogic.gdx.graphics.Texture
+
+/**
+ * Created by Vladislav on 3/1/2015.
+ */
+class SomeClass {
+  var img = new Texture("badlogic.jpg")
+  def getImg = img;
+}
