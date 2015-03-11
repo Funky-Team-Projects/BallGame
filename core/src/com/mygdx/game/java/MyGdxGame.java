7af4b645bcf8d24329f863485fd12ffa75ce194d
Nelosh <asakura.jo@gmail.com>
2015-03-11 22:52:28 +0200

    Camera is bounded to ball

--- core/src/com/mygdx/game/java/MyGdxGame.java
+++ core/src/com/mygdx/game/java/MyGdxGame.java
@@ -3,6 +3,7 @@ package com.mygdx.game.java;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -15,6 +16,10 @@ import com.mygdx.game.scala.Pos;
 import com.mygdx.game.scala.World;
 
 public class MyGdxGame extends ApplicationAdapter {
+
+    final int WIDTH = 1200;
+    final int HEIGHT = 900;
+
 	SpriteBatch batch;
 	Texture img;
     Stage stage;
@@ -22,16 +27,20 @@ public class MyGdxGame extends ApplicationAdapter {
     Ball ball;
     Block block;
     BallInputProcessor inputProcessor;
+    OrthographicCamera camera;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
-        viewport = new StretchViewport(700, 500); /**Creates a viewport with the responding size*/
         ball = new Ball();
 
         block = new Block(new Pos(0,0), new Pos(220,40));
         World.add(block);
+
+        camera = new OrthographicCamera();
+        camera.position.set(ball.getX(),ball.getY(),0);
+        viewport = new StretchViewport(WIDTH, HEIGHT, camera);
 
         stage = new Stage(viewport, batch);       /**Creates stage on our viewport with our batch*/
         stage.addActor(ball);                     /**add actor to the stage*/
@@ -48,12 +57,16 @@ public class MyGdxGame extends ApplicationAdapter {
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	//	batch.begin();
+
+        /**cameras modifications, poorly realizes camera modification*/
+        camera.position.set(ball.getX() + WIDTH/3,ball.center().y(),0);
+        stage.getViewport().setCamera(camera);
+
         World.draw(batch);
-        stage.draw();                            /**Draws all actors*/
+        stage.getViewport().setCamera(camera);
+        stage.draw();
         stage.act(Gdx.graphics.getDeltaTime());  /**Calls method acts to all actors*/
-	//	batch.draw(img, 0, 0);
-	//	batch.end();
+
 	}
 
     @Override
